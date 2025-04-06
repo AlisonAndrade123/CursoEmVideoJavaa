a87be8945a9b96560b635ae87482f6a3f66d56c1
AlisonAndrade123 <andradealison291@gmail.com>
2025-04-06 19:33:09 -0300

    Enviando atividades 2

---
+++ src/Aula11.java
@@ -0,0 +1,15 @@
+public class Aula11 {
+    public static void main(String[] args) {
+        int cc = 0;
+        while (cc < 10){
+            cc++;
+            if (cc == 2 || cc == 4){
+                continue;
+            }
+            if (cc == 7){
+                break;
+            }
+            System.out.println("Cambalhota " + cc);
+        }
+    }
+}
---
+++ src/Aula12.java
@@ -0,0 +1,9 @@
+public class Aula12 {
+    public static void main(String[] args) {
+        int cc = 0;
+        do {
+            System.out.println("Cambalhota");
+            cc++;
+        } while (cc < 6);
+    }
+}
